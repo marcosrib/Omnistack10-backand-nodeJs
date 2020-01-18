37fa9b16dcc19bab543289996e34af6a846f93c9
marcos <marcos.rib.sousa@gmail.com>
2020-01-17 21:01:29 -0300

    implementando socket io

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
-mongoose.connect('mongodb+srv://root:admin@cluster0-e4q6l.mongodb.net/omnistack10?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://root:admin@cluster0-e4q6l.mongodb.net/omnistack10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 
 const app = express();
+const server = http.Server(app);
+setupWebsocket(server);
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333)
+server.listen(3333)
