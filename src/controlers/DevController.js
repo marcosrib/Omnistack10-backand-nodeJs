d3f1f8f2af8e475738e54ec496787f4a3a2d0e82
marcos <marcos.rib.sousa@gmail.com>
2020-01-19 15:09:02 -0300

    finalizando backend

--- src/controlers/DevController.js
+++ src/controlers/DevController.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 const parseStringAsArray = require('../utils/parseStringAsArray')
+
+const { findConections, sendMessage } = require('../websocket')
 module.exports = {
 
     async index(req, res) {
@@ -13,16 +15,18 @@ module.exports = {
 
     async store(req, res) {
         const { github_username, techs, longitude, latitude } = req.body;
-
+        
+         
         let dev = await Dev.findOne(
             { github_username }
         )
+    
         if (!dev) {
             const response = await axios.get(`https://api.github.com/users/${github_username}`);
             const { name = login, avatar_url, bio } = response.data;
-
-            const techsArray = parseStringAsArray(techs);
             
+            const techsArray = parseStringAsArray(techs);
+            console.log(techsArray);
             const location = {
                 type: 'Point',
                 coordinates: [longitude, latitude]
@@ -38,10 +42,19 @@ module.exports = {
                 techs: techsArray,
                 location,
             })
-            
-            Dev.collection.createIndex( { location : "2dsphere" } )
 
+            Dev.collection.createIndex({ location: "2dsphere" })
+
+            const sendSocketMessageTo = findConections(
+                { latitude, longitude },
+                techsArray
+            )
+            console.log(sendSocketMessageTo);
+            
+            sendMessage(sendSocketMessageTo, 'new-dev', dev)
         }
+
+       
         return res.json(dev);
     }
 }
--- src/websocket.js
+++ src/websocket.js
@@ -1,10 +1,12 @@
 const socketio = require('socket.io');
 const parseStringAsArray = require('./utils/parseStringAsArray')
+const calculateDistance = require('./utils/calculateDistance')
 
+let io;
 const connections = [];
 
 exports.setupWebsocket = (server) => {
-    const io = socketio(server);
+    io = socketio(server);
     io.on('connection', socket => {
 
         const { latitude, longitude, techs } = socket.handshake.query;
@@ -16,6 +18,21 @@ exports.setupWebsocket = (server) => {
                longitude: Number(longitude)
            },
            techs: parseStringAsArray(techs)
-       })
-    })
+       });
+    });
+}
+exports.findConections = (coordinates, techs) => {
+   return connections.filter(connection => {
+       console.log(techs);
+    const t =   connection.techs.some(item => techs.includes(item))
+    console.log(t);
+       return calculateDistance(coordinates, connection.coordinates) < 10
+      // && connection.techs.some(item => techs.includes(item))
+   })
+}
+
+exports.sendMessage = (to, message, data ) => {
+    to.forEach(connection => {
+      io.to(connection.id).emit(message, data)
+    });
 }
